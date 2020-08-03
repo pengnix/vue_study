62556c71775680cc31809e986c9feb14b91ccdb5
pengnix <superpengnix@gmail.com>
2020-08-03 16:17:22 +0800

    es6转es5

---
+++ vueproject/02-webpack/04-webpack配置vue/src/main.js
@@ -0,0 +1,11 @@
+const {add, mul} = require('./js/mathUtils');
+console.log(add(10, 10));
+
+import  {name} from "./js/info"
+
+console.log(name);
+
+require('./css/normal.css')
+require('./css/special.less')
+
+document.writeln('<h2>run run run</h2>')
