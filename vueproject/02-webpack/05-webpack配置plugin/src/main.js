3e9e2c475ee9a18722d7ab67d0631d841efc7ae9
pengnix <superpengnix@gmail.com>
2020-08-04 10:49:56 +0800

    plugin和本地服务器搭建

---
+++ vueproject/02-webpack/05-webpack配置plugin/src/main.js
@@ -0,0 +1,24 @@
+const {add, mul} = require('./js/mathUtils');
+console.log(add(10, 10));
+
+import  {name} from "./js/info"
+
+console.log(name);
+
+require('./css/normal.css')
+require('./css/special.less')
+
+document.writeln('<h2>run run run</h2>')
+
+import Vue from 'vue'
+
+// import App from './vue/app'
+import App from './vue/App.vue'
+
+new Vue({
+    el:"#app",
+    template:`<App/>`,
+    components:{
+        App
+    }
+})
---
+++ vueproject/02-webpack/05-webpack配置plugin/webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifysWebpackPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+        // publicPath: 'dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            //小于limit，将图片编译成base64字符串形式
+                            //大于limit，将使用file-loader
+                            limit: 30000,
+                            name: 'img/[name].[hash:8].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                use: [{
+                    loader: "vue-loader" // creates style nodes from JS strings
+                }]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.css', '.vue'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    plugins: [
+        new webpack.BannerPlugin('最终版权归hello所有'),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new UglifysWebpackPlugin()
+    ],
+    devServer: {
+        contentBase:'./dist',
+        inline:true
+    }
+}
