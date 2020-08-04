e95e5b6234c4ca13754f9fc8663abb3e694e9a3b
pengnix <superpengnix@gmail.com>
2020-08-04 13:27:19 +0800

    配置文件分离

---
+++ vueproject/02-webpack/06-webpack配置分离/build/prod.config.js
@@ -0,0 +1,9 @@
+const UglifysWebpackPlugin = require('uglifyjs-webpack-plugin')
+const webpackMerge = require('webpack-merge')
+const baseConfig = require('./base.config')
+
+module.exports = webpackMerge.merge(baseConfig,{
+    plugins: [
+        new UglifysWebpackPlugin()
+    ]
+})
