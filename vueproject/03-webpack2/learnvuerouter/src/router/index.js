fbea29e651c18a704e08167cbb800f24cb9a5e97
pengnix <superpengnix@gmail.com>
2020-09-02 13:17:57 +0800

    路由懒加载

--- vueproject/03-webpack2/learnvuerouter/src/router/index.js
+++ vueproject/03-webpack2/learnvuerouter/src/router/index.js
@@ -1,35 +1,39 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
-import Home from '../components/Home'
-import About from '../components/About'
-import User from '../components/User'
+// import Home from '../components/Home'
+// import About from '../components/About'
+// import User from '../components/User'
+
+const Home = () => import('../components/Home')
+const About = () => import('../components/About')
+const User = () => import('../components/Home')
 
 
 Vue.use(VueRouter)
 
 const routes = [
     {
-        path:'/',
-        redirect:'/home'
+        path: '/',
+        redirect: '/home'
     },
     {
-        path:'/home',
-        component:Home
+        path: '/home',
+        component: Home
     },
     {
-        path:'/about',
-        component:About
+        path: '/about',
+        component: About
     },
     {
-        path:'/user/:userId',
-        component:User
+        path: '/user/:userId',
+        component: User
     }
 ]
 
 const router = new VueRouter({
     routes,
-    mode:'history',
-    linkActiveClass:'active'
+    mode: 'history',
+    linkActiveClass: 'active'
 })
 
 export default router
