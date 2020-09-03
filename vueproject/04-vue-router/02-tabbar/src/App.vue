d801f44bbbb5f10d8ece423eff0ac68c4b92eabf
pengnix <superpengnix@gmail.com>
2020-09-03 16:45:06 +0800

    refactor

--- vueproject/04-vue-router/02-tabbar/src/App.vue
+++ vueproject/04-vue-router/02-tabbar/src/App.vue
@@ -3,40 +3,17 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
-    <tab-bar>
-      <tab-bar-item path="/home" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/home.png" />
-        <img slot="item-icon-active" src="./assets/img/tabbar/home_active.png" />
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item path="/category" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/category.png" />
-        <img slot="item-icon-active" src="./assets/img/tabbar/category_active.png" />
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item path="/cart" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/shopcart.png" />
-        <img slot="item-icon-active" src="./assets/img/tabbar/shopcart_active.png" />
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item path="/profile" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/profile.png" />
-        <img slot="item-icon-active" src="./assets/img/tabbar/profile_active.png" />
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import TabBar from "./components/tabbar/TabBar";
-import TabBarItem from "./components/tabbar/TabBarItem";
+import MainTabBar from "./components/MainTabBar";
 
 export default {
   name: "App",
   components: {
-    TabBar,
-    TabBarItem,
+    MainTabBar
   },
 };
 </script>
