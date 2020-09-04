559d23dca92ddebacfe56506b19c64f99d6a0078
pengnix <superpengnix@gmail.com>
2020-09-04 16:47:22 +0800

    vuex使用

---
+++ vueproject/05-promise/02-vuex-demo/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>{{message}}</h1>
+    <h1>{{$store.state.counter}}</h1>
+    <button @click="addition">+</button>
+    <button @click="subtraction">-</button>
+    <hello-vuex></hello-vuex>
+  </div>
+</template>
+
+<script>
+import HelloVuex from "./components/HelloVuex";
+
+export default {
+  name: "App",
+  components: {
+    HelloVuex,
+  },
+  data() {
+    return {
+      message: "App组件",
+      counter: 0,
+    };
+  },
+  methods: {
+    addition() {
+      this.$store.commit("increment");
+    },
+    subtraction() {
+      this.$store.commit("decrement");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vueproject/05-promise/02-vuex-demo/src/components/HelloVuex.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{$store.state.counter}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name:"HelloVuex"
+}
+</script>
+<style scoped>
+
+</style>
