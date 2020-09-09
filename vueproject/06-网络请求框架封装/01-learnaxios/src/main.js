7a33d3d5405d06b88d94e0fb96846833c89373ea
pengnix <superpengnix@gmail.com>
2020-09-09 13:19:03 +0800

    axios 配置信息 封装 以及拦截器

--- vueproject/06-网络请求框架封装/01-learnaxios/src/main.js
+++ vueproject/06-网络请求框架封装/01-learnaxios/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import axios from 'axios'
+import { request } from './network/request'
 
 Vue.config.productionTip = false
 
@@ -40,13 +41,53 @@ new Vue({
 //   console.log(result[1])
 // })
 
-axios.all([axios({ url: 'http://123.207.32.32:8000/home/multidata' }), axios({
-  url: 'http://123.207.32.32:8000/home/data',
-  params: {
-    type: 'pop',
-    page: 1
-  },
-})]).then(axios.spread((res1,res2) => {
-  console.log(res1)
-  console.log(res2)
-}))
+//全局
+// axios.defaults.baseURL = 'http://123.207.32.32:8000'
+// axios.defaults.timeout = 5000
+
+// axios.all([axios({
+//   url: '/home/multidata'
+// }), axios({
+//   url: '/home/data',
+//   params: {
+//     type: 'pop',
+//     page: 1
+//   },
+// })]).then(axios.spread((res1, res2) => {
+//   console.log(res1)
+//   console.log(res2)
+// }))
+
+// const instance1 = axios.create({
+//   baseURL:'http://123.207.32.32:8000',
+//   timeout:5000
+// })
+
+// instance1({
+//   url:'/home/multidata'
+// }).then(res=>{
+//   console.log(res)
+// })
+
+// request({
+//   url: '/home/multidata'
+// }, success => console.log(success),
+//   failture => console.log(failture)
+// )
+
+// request({
+//   baseConfig: '/home/multidata',
+//   success: function (res) {
+//     console.log(res)
+//   },
+//   failure: function (err) {
+//     console.log(err)
+//   }
+// }
+// )
+
+request({ url: '/home/multidata' }).then(res => {
+  console.log(res)
+}).catch(err => {
+  console.log(err)
+})
