f955428a7078b6f3ed3c6ab419326bb0b067b86d
pengnix <superpengnix@gmail.com>
2020-09-10 16:07:05 +0800

    数据获取和轮播图展示

--- vueproject/06-网络请求框架封装/02-hellomall/src/views/home/Home.vue
+++ vueproject/06-网络请求框架封装/02-hellomall/src/views/home/Home.vue
@@ -1,24 +1,41 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"/>
   </div>
 </template>
 
 <script>
-
-import NavBar from 'components/common/navbar/NavBar'
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home.js";
+import HomeSwiper from './childComps/HomeSwiper'
 export default {
   name: "Home",
   components: {
-    NavBar
-  }
+    NavBar,
+    HomeSwiper
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
 };
 </script>
 
 <style scoped>
-/* @import "assets/css/base.css"; */
-.home-nav{
-  background-color:var(--color-tint);
-  color:#fff
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
 }
 </style>
