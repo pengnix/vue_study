ed18c09cb9f2dbc05aeba56dcb7651b0da9dfa2c
pengnix <superpengnix@gmail.com>
2020-09-10 13:18:36 +0800

    路径别名和tabbar引入

--- vueproject/06-网络请求框架封装/02-hellomall/src/App.vue
+++ vueproject/06-网络请求框架封装/02-hellomall/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
+    <router-view></router-view>
+    <main-tab-bar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainTabBar from "components/content/mainTabber/MainTabBar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MainTabBar,
+  },
+};
 </script>
 
 <style>
+@import "assets/css/base.css";
 </style>
--- vueproject/06-网络请求框架封装/02-hellomall/src/views/home/Home.vue
+++ vueproject/06-网络请求框架封装/02-hellomall/src/views/home/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
+    <h2>我是首页</h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
-}
+  name: "Home"
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
