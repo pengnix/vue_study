390af0148dd277add8393c4437bf1dcb54e9f232
pengnix <superpengnix@gmail.com>
2020-09-10 14:25:42 +0800

    首页导航栏封装

--- vueproject/06-网络请求框架封装/02-hellomall/src/views/home/Home.vue
+++ vueproject/06-网络请求框架封装/02-hellomall/src/views/home/Home.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>
-    <h2>我是首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
   </div>
 </template>
 
 <script>
+
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar
+  }
 };
 </script>
 
 <style scoped>
+/* @import "assets/css/base.css"; */
+.home-nav{
+  background-color:var(--color-tint);
+  color:#fff
+}
 </style>
