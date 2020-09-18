69c136d9943b6e42a7b2b2e015ca04681f94f725
pengnix <superpengnix@gmail.com>
2020-09-18 16:20:15 +0800

    详情页导航页封装

--- vueproject/06-网络请求框架封装/02-hellomall/src/views/detail/Detail.vue
+++ vueproject/06-网络请求框架封装/02-hellomall/src/views/detail/Detail.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    <h2>我是Detail</h2>
+  <div class="detail">
+    <detail-nav-bar></detail-nav-bar>
   </div>
 </template>
 
 <script>
+import DetailNavBar from "views/detail/childComps/DetailNavBar"
+
 export default {
   name: "Detail",
   data() {
@@ -15,6 +17,9 @@ export default {
   created() {
     this.iid = this.$route.params.iid;
   },
+  components: {
+    DetailNavBar
+  }
 };
 </script>
 
