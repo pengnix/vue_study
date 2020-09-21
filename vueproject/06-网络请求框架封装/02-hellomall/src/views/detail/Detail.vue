800ace8cf8e23fe6a8053a679f81ee182a7bde48
pengnix <superpengnix@gmail.com>
2020-09-21 14:05:53 +0800

    商品基本信息展示

--- vueproject/06-网络请求框架封装/02-hellomall/src/views/detail/Detail.vue
+++ vueproject/06-网络请求框架封装/02-hellomall/src/views/detail/Detail.vue
@@ -1,34 +1,41 @@
 <template>
   <div class="detail">
     <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :topImages="topImages"/>
+    <detail-swiper :topImages="topImages" />
+    <detail-base-info :goods="goods"/>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "views/detail/childComps/DetailNavBar";
-import { getDetail } from "network/detail.js";
-import DetailSwiper from "views/detail/childComps/DetailSwiper"
+import { getDetail, Goods } from "network/detail.js";
+import DetailSwiper from "views/detail/childComps/DetailSwiper";
+import DetailBaseInfo from "views/detail/childComps/DetailBaseInfo";
+
 
 export default {
   name: "Detail",
   data() {
     return {
       iid: null,
-      topImages:[]
+      topImages: [],
+      goods:{}
     };
   },
   created() {
     this.iid = this.$route.params.iid;
     getDetail(this.iid).then((res) => {
       console.log(res);
-      this.topImages = res.result.itemInfo.topImages;
+      const data = res.result;
+      this.topImages = data.itemInfo.topImages;
+      this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
       // console.log(this.topImages)
     });
   },
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo
   },
 };
 </script>
