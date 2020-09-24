195e15b6fbc575e0084fbfb569570ffae1e3117f
pengnix <superpengnix@gmail.com>
2020-09-24 14:33:45 +0800

    滚动内容对应相应的标题

--- vueproject/06-网络请求框架封装/02-hellomall/src/views/detail/Detail.vue
+++ vueproject/06-网络请求框架封装/02-hellomall/src/views/detail/Detail.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="detail">
-    <detail-nav-bar class="detail-nav" @titleClick="titleClick"></detail-nav-bar>
-    <scroll class="content" :pull-up-load="true" @pullingUp="loadMore" :probe-type="3" ref="scroll">
+    <detail-nav-bar class="detail-nav" @titleClick="titleClick" ref="nav"></detail-nav-bar>
+    <scroll
+      class="content"
+      :pull-up-load="true"
+      @pullingUp="loadMore"
+      :probe-type="3"
+      ref="scroll"
+      @scroll="contentScroll"
+    >
       <detail-swiper :top-images="topImages" />
       <detail-base-info :goods="goods" />
       <detail-shop-info :shop="shop" />
@@ -48,7 +55,8 @@ export default {
       commentInfo: {},
       recommends: [],
       themeTopYs: [],
-      getThemeTopY:[]
+      getThemeTopY: [],
+      currentIndex: 0,
     };
   },
   mixins: [itemListenerMixin],
@@ -56,12 +64,26 @@ export default {
     loadMore() {},
     imageLoad() {
       // this.$refs.scroll.refresh();
-      this.getThemeTopY()
+      this.getThemeTopY();
       this.refresh();
     },
     titleClick(index) {
       console.log("index=" + index);
       this.$refs.scroll.scrollTo(0, -this.themeTopYs[index], 100);
+    },
+    contentScroll(position) {
+      const pY = -position.y;
+      let len = this.themeTopYs.length;
+      for (let i = 0; i < len - 1; i++) {
+        if (
+          this.currentIndex !== i &&
+          pY >= this.themeTopYs[i] &&
+          pY < this.themeTopYs[i + 1]
+        ) {
+          this.currentIndex = i;
+          this.$refs.nav.currentIndex = this.currentIndex;
+        }
+      }
     },
   },
   created() {
@@ -97,14 +119,15 @@ export default {
       // console.log(res);
       this.recommends = res.data.list;
     });
-    this.getThemeTopY = debounce(()=>{
+    this.getThemeTopY = debounce(() => {
       this.themeTopYs = [];
       this.themeTopYs.push(0);
       this.themeTopYs.push(this.$refs.params.$el.offsetTop);
       this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
       this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
-      console.log("----")
-    },200)
+      this.themeTopYs.push(Number.MAX_VALUE);
+      console.log("----");
+    }, 200);
   },
   components: {
     DetailNavBar,
