cbd71500a33e05375d18003f1ddfed686569e969
pengnix <superpengnix@gmail.com>
2020-09-27 15:43:06 +0800

    实现图片懒加载

--- vueproject/06-网络请求框架封装/02-hellomall/src/main.js
+++ vueproject/06-网络请求框架封装/02-hellomall/src/main.js
@@ -4,11 +4,15 @@ import router from "./router";
 import store from "./store";
 import toast from "components/common/toast";
 import FastClick from "fastclick"
+import VueLazyLoad from "vue-lazyload"
 
 Vue.config.productionTip = false;
 Vue.prototype.$bus = new Vue();
 Vue.use(toast)
 FastClick.attach(document.body)
+Vue.use(VueLazyLoad,{
+  loading:require("./assets/img/common/placeholder.png")
+})
 
 new Vue({
   render: h => h(App),
