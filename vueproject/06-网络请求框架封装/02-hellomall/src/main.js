a988d39e068fdab694151c3701631d8ab826babf
pengnix <superpengnix@gmail.com>
2020-09-27 15:25:56 +0800

    添加fastclick解决移动端300ms延迟

--- vueproject/06-网络请求框架封装/02-hellomall/src/main.js
+++ vueproject/06-网络请求框架封装/02-hellomall/src/main.js
@@ -3,10 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import toast from "components/common/toast";
+import FastClick from "fastclick"
 
 Vue.config.productionTip = false;
 Vue.prototype.$bus = new Vue();
 Vue.use(toast)
+FastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
